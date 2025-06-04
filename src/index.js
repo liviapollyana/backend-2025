fa0802a08c7876898fcd1d0afc0cc8a0b641955a
lívia <162451674+liviapollyana@users.noreply.github.com>
2025-06-04 12:40:30 +0000

    [feat] Inicial Configuration

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const server = express();
+
+const PORT = process.env.PORT || 3000;
+
+server.get("/", (req, res)=>{
+    res.status(200).send("aaaaaaaaaaaaaaaaaa");
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
